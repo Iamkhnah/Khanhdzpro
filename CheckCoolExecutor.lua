2c2b7efdc39a994d5c1b07af4373838e8a13b769
ItOnltz <hohohubbro@gmail.com>
2024-10-06 22:34:13 +0700

    Create CheckCoolExecutor.lua

---
+++ CheckCoolExecutor.lua
@@ -0,0 +1,32 @@
+
+local a, b = pcall(function()
+    local old = getrawmetatable(game)
+    local v1 = old.__namecall
+    setreadonly(old, false)
+    old.__namecall = newcclosure(function(v2, ...)
+        local met = getnamecallmethod()
+        local table = {...}
+        if met == 'FireServer' or met == 'InvokeServer' then
+            print("Name: " .. v2.Name)
+            print("Arg2: " .. table[1])
+        end
+        return v1(v2, unpack(table))
+    end)
+    local oldNamecall
+    oldNamecall = hookfunction(game.__namecall, function(self, ...)
+        local method = getnamecallmethod()
+        if method == "FireServer" or method == "InvokeServer" then
+            print("Name Method: ", method)
+            print("Obj:", self)
+            print("func: ", ...)
+        end
+        return oldNamecall(self, ...)
+    end)
+end)
+if a then
+    while true do
+        print("Bro Executor so cool!!")
+    end
+else
+    game.Players.LocalPlayer:kick("LoL Executor so trash")
+end
